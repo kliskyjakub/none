5fe3022cacc81b1655f4577d9ae4b4aab8e91bc6
Jakub Kliský <jakubko.klisky@gmail.com>
2018-07-01 16:00:04 +0200

    1.2.0 STABLE WS Api Complete User Data Output

--- views/ws/index.php
+++ views/ws/index.php
@@ -1,35 +1,55 @@
 <?php
 
 use yii\data\ArrayDataProvider;
-
+use yii\grid\GridView;
+use yii\widgets\DetailView;
 echo '<h1>WebSupport API</h1><h2>User Data</h2>';
 
-foreach ($userData as $user_key => $user_value) {
-    print('<b>' . $user_key . '</b><br/>');
-    var_dump($user_value);
-    echo '<br/><br/>';
-}
+echo DetailView::widget([
+    'model' => $userData,
+    'attributes' => [
+        'id',
+        'login',
+        'parentID',
+        'active',
+        'createTime',
+        'group',
+        'email',
+        'phone',
+        'sknicHandle',
+        'contactPerson',
+        'resellerToken',
+        'credit',
+        'verifyUrl'
+    ]]);
+
+echo '<h4>Billing</h4>';
+$billingDataProvider = new ArrayDataProvider([
+    'allModels' => $userData['billing'],
+]);
+echo GridView::widget([
+    'dataProvider' => $billingDataProvider,
+]);
+
+echo '<h4>Market</h4>';
+echo DetailView::widget([
+    'model' => $userData['market']
+]);
 
 echo '<h2>Service Data</h2>';
 
-foreach ($serviceData as $service_key => $service_value) {
-    print('<h3>' . $service_key . '</h3>');
-    foreach ($service_value as $service_value_key => $service_value_value) {
-        print('<b>' . $service_value_key . '</b><br/>');
-        var_dump($service_value_value);
-        echo '<br/><br/>';
-    }
-    echo '<br/><br/>';
-}
+$serviceDataProvider = new ArrayDataProvider([
+    'allModels' => $serviceData,
+]);
+echo GridView::widget([
+    'dataProvider' => $serviceDataProvider,
+]);
 
 echo '<h2>Invoice Data</h2>';
 
-foreach ($invoiceData as $invoice_key => $invoice_value) {
-    print('<h3>' . $invoice_key . '</h3>');
-    foreach ($invoice_value as $invoice_value_key => $invoice_value_value) {
-        print('<b>' . $invoice_value_key . '</b><br/>');
-        var_dump($invoice_value_value);
-        echo '<br/><br/>';
-    }
-    echo '<br/><br/>';
-}
+$invoiceDataProvider = new ArrayDataProvider([
+    'allModels' => $invoiceData,
+]);
+echo GridView::widget([
+    'dataProvider' => $invoiceDataProvider,
+]);
